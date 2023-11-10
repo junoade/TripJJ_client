786c989fa8f0c61b05e60d36ff0b99bad08d7235
junoade <ajchoi0928@gmail.com>
2023-11-10 13:42:43 +0900

    init: vue-project

---
+++ src/views/TheElectricChargingStation.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container text-center mt-3">
+    <div class="alert alert-success" role="alert">전기차 충전소</div>
+    <table class="table table-hover">
+      <thead>
+        <tr class="text-center">
+          <th scope="col">충전소명</th>
+          <th scope="col">충전소ID</th>
+          <th scope="col">충전기상태</th>
+          <th scope="col">위치</th>
+          <th scope="col">위도</th>
+          <th scope="col">경도</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="text-center">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+mark.purple {
+  background: linear-gradient(to top, #c354ff 20%, transparent 30%);
+}
+</style>
