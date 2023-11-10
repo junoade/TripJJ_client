37fcd493f20fb623cd68910bd6227dcf6f0915f4
junoade <ajchoi0928@gmail.com>
2023-11-10 15:14:08 +0900

    feat: bootstrap 설정

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// bootstrap 사용을 위해
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
