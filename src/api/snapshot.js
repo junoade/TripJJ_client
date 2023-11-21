9c0d10a29a2f74bed38003135399520ee33f57e5
junoade <ajchoi0928@gmail.com>
2023-11-21 09:29:23 +0900

    init: 스냅샷 게시판 api 설계

---
+++ src/api/snapshot.js
@@ -0,0 +1,48 @@
+import { localAxios } from "@/util/http-commons";
+
+const local = localAxios();
+
+/**
+ * 사용자가 스냅샷 게시판에 올린 스토리를 서버로 전송한다.
+ * An User uploads a story on SnapShot Board
+ * @param {*} param
+ * @param {*} success
+ * @param {*} fail
+ */
+function uploadStory(param, success, fail) {}
+
+/**
+ * 사용자가 스냅샷 게시판에 올린 스토리를 수정한다.
+ * An User modifies the story on SnapShot Board which is once uploaded before
+ * @param {*} param
+ * @param {*} success
+ * @param {*} fail
+ */
+function modifyStory(param, success, fail) {}
+
+/**
+ * 사용자가 자신이 올린 스토리를 삭제한다.
+ * An User deletes its own story
+ * @param {*} param
+ * @param {*} success
+ * @param {*} fail
+ */
+function deleteStory(param, success, fail) {}
+
+/**
+ * 사용자 아이디(userId)를 바탕으로 자신이 업로드한 스토리 목록을 조회한다
+ * An User get list of own stories which are uploaded before
+ * @param {*} param
+ * @param {*} success
+ * @param {*} fail
+ */
+function findStoryByUserId(param, success, fail) {}
+
+/**
+ * 스토리 아이디(storyId)를 바탕으로 특정 스토리를 조회한다.
+ * An User get a specified story by storyId
+ * @param {*} param
+ * @param {*} success
+ * @param {*} fail
+ */
+function findStoryByStoryId(param, success, fail) {}
